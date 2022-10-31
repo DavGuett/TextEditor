cc26b83efa47ec53faf1d4a8725f3e0d7ff35fff
DavGuett <soulsec@protonmail.com>
2022-10-30 21:17:53 -0300

    projeto criado, menu feito

---
+++ TextEditor/Program.cs
@@ -0,0 +1,37 @@
+﻿static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+    int option = int.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0:
+            System.Environment.Exit(0);
+            break;
+        case 1:
+            Open();
+            break;
+        case 2:
+            Edit();
+            break;
+        default:
+            Menu();
+            break;
+    }
+}
+
+static void Open()
+{
+    
+}
+
+static void Edit()
+{
+    
+}
+
+Menu();

4fbac778815177d01ba12f4da81a4076e5894d54
DavGuett <soulsec@protonmail.com>
2022-10-31 21:19:19 -0300

    projeto finalizado

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -26,7 +26,18 @@
 
 static void Open()
 {
-    
+    Console.Clear();
+    Console.WriteLine("Qual o caminho do arquivo?");
+    string path = Console.ReadLine();
+
+    using (var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    }
+
+    Console.ReadLine();
+    Menu();
 }
 
 static void Edit()
